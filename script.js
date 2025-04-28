ff25b0546a07057ac56b7fe6bc3198073f1436c3
Asadullah1-maker <masadrana2002@gmail.com>
2025-04-28 09:32:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementById('toggleButton');
+toggleButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+
+    console.log('Email submitted:', email);
+    alert('Thank you for your submission!');
+    form.reset();
+});
