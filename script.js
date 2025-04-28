f42bb7aad94918eac05bed6740762759d15efca1
Asadullah1-maker <masadrana2002@gmail.com>
2025-04-28 10:18:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const form = document.getElementById('contact-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
-
     console.log('Email submitted:', email);
     alert('Thank you for your submission!');
     form.reset();
